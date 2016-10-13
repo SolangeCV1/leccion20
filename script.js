2fd6d67058b30aecc1ebea7ffffcb8f48544482e
solangecv1 <sol.cayuqueo@gmail.com>
2016-10-12 22:49:55 -0300

    ejercicio leccion20

---
+++ script.js
@@ -0,0 +1,16 @@
+// tu codigo va aca
+
+// 1er Ejercicio
+
+function callbackPromedio(a,b,c,callback)
+{
+	var promedio = ((a+b+c)/3);
+	return callback(promedio);
+}
+// 2do Ejercicio
+
+function segundero()
+{	 
+	 document.getElementById('titulo').innerHTML = '5';
+	 
+}
